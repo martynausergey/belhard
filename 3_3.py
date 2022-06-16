56d0a1311c5ec2664ac070e0dbd6a1e17f27c102
martynausergey <sergeym767@gmail.com>
2022-06-17 01:15:54 +0300

    initial commit

---
+++ 3_3.py
@@ -0,0 +1,19 @@
+# 1
+name = input("Имя - ")
+age = input("Возраст - ")
+city = input("Город - ")
+print(f"Добрый день {name}! Тебе {age} года. Ты из города {city}")
+
+# 2
+name = input("Имя - ")
+age = input("Возраст - ")
+city: str = input("Город - ")
+print("Добрый день {}! Тебе {} года. Ты из города {}".format(name, age, city))
+
+# 3
+name = input("Имя - ")
+age = input("Возраст - ")
+city = input("Город - ")
+print("Добрый день %s. Тебе %d года. Ты из города %s." % (name, age, city))
+
+
