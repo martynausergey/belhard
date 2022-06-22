00ae80b71ca729d5c0dc2326dc04f805bf6553b5
martynausergey <107396201+martynausergey@users.noreply.github.com>
2022-06-22 18:24:01 +0300

    Create 4_3.py

---
+++ 4_3.py
@@ -0,0 +1,4 @@
+users = int(input("Новых пользователей: "))
+dict = {x: {"Имя пользователя": input(f"{x} Имя пользователя: "),"email пользователя": input(f"{x} Email пользователя: ")}
+for x in range (1, users + 1)}
+print (dict)
