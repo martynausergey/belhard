e14e6cd0b7e886d03e0fce08bdc3e255b55ef547
martynausergey <107396201+martynausergey@users.noreply.github.com>
2022-06-22 19:26:51 +0300

    Delete 4_3.py

--- 4_3.py
+++
@@ -1,4 +0,0 @@
-users = int(input("Новых пользователей: "))
-dict = {x: {"Имя пользователя": input(f"{x} Имя пользователя: "),"email пользователя": input(f"{x} Email пользователя: ")}
-for x in range (1, users + 1)}
-print (dict)
