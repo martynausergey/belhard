ad56a898ec6e52473413d7974de5d656a035cd00
martynausergey <107396201+martynausergey@users.noreply.github.com>
2022-06-26 18:43:21 +0300

    Update 5_2.py

--- 5_2.py
+++ 5_2.py
@@ -1,12 +1,12 @@
 a = int(input("Введите число a: "))
-while not a.isdigit():
-	a = int(input("Не является числом. Введите число: "))
-	a = int(a)
+#while not a.isdigit():
+	#a = int(input("Не является числом. Введите число: "))
+	#a = int(a)
 q = input("Введите действие (+, -, *, /, **): ")
 b = int(input("Введите число b: "))
-while not b.isdigit():
-	b = int(input("Не является числом. Введите число: "))
-	b = int(b)
+#while not b.isdigit():
+	#b = int(input("Не является числом. Введите число: "))
+	#b = int(b)
 if q == "+":
 	result = a + b
 elif q== "-":
