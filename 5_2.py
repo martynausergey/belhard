9ae4669f40aa42c7885c2fc83ca1f7c6262b27d3
martynausergey <107396201+martynausergey@users.noreply.github.com>
2022-06-26 19:20:13 +0300

    Update 5_2.py

--- 5_2.py
+++ 5_2.py
@@ -1,12 +1,15 @@
-a = int(input("Введите число a: "))
-#while not a.isdigit():
-	#a = int(input("Не является числом. Введите число: "))
-	#a = int(a)
+a = input("Введите число a: ")
+while not a.isdigit():
+	a = input("Не является числом. Введите число: ")
+a = int(a)
 q = input("Введите действие (+, -, *, /, **): ")
-b = int(input("Введите число b: "))
-#while not b.isdigit():
-	#b = int(input("Не является числом. Введите число: "))
-	#b = int(b)
+list_q = ["+", "-", "*", "/", "**"]
+while q not in list_q:
+	q = input("Неправильное действие, введите заново: ")
+b = input("Введите число b: ")
+while not b.isdigit():
+	b = input("Не является числом. Введите число: ")
+b = int(b)
 if q == "+":
 	result = a + b
 elif q== "-":
@@ -17,6 +20,4 @@ elif q == "/":
 	result = a / b
 elif q == "**":
 	result == a ** b
-else:
-	result = a % b
 print("Результат :", result)
