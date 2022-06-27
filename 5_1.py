2807ede2643978c07455e39e16c86722a13d8d88
martynausergey <107396201+martynausergey@users.noreply.github.com>
2022-06-27 10:44:44 +0300

    Update 5_1.py

--- 5_1.py
+++ 5_1.py
@@ -1,3 +1,5 @@
+# 1 Вариант
+# Вывести первые n чисел, кратные m и больше k
 n = int(input("Введите число n: "))
 m = int(input("Введите число m: "))
 k = int(input("Ввещиье число k: "))
@@ -8,3 +10,14 @@ while count != n:
 		print(x)
 		count += 1
 	x += 1
+# 2 Вариант 
+n = int(input("Введите число n: "))
+m = int(input("Введите число m: "))
+k = int(input("Ввещиье число k: "))
+x = k
+count = 0
+while count < 0:
+	if not x % m:
+		count += 1
+		print(i)
+	i += 1
