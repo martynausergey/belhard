de302bbe40dc9245de684cd77905ede11762853d
martynausergey <sergeym767@gmail.com>
2022-06-29 13:46:08 +0300

    initial commit

--- 6_3.py
+++ 6_3.py
@@ -1,5 +1,7 @@
 lst = [1, 2, 3, 4, 5, 6, 7, 8]
 n = int(input("Сместить список на: "))
+
+
 def shift(lst_n):
     shifted_n = lst_n
     counter = 0
@@ -7,5 +9,6 @@ def shift(lst_n):
         shifted_n.insert(0, shifted_n.pop())
         counter += 1
     return shifted_n
-print(shift(lst))
 
+
+print(shift(lst))
