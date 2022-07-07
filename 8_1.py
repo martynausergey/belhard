9b50f0ca4111acd6fa956d3b01aa5fa6c030d187
martynausergey <107396201+martynausergey@users.noreply.github.com>
2022-07-07 10:56:15 +0300

    Update 8_1.py

--- 8_1.py
+++ 8_1.py
@@ -1,3 +1,4 @@
+# 1 Вариант
 n = int(input("Введите глубину: "))
 
 
@@ -10,3 +11,29 @@ def pascal_triangle(rows):
 
 print(pascal_triangle(n))
 
+#2 Вариант
+n: int = int(input("Глубина"))
+triangle: list = []
+lst: list = []
+for i in range(n+n-1):
+    lst.append(0)
+for i in range(n):
+    triangle.append(lst.copy())
+print(triangle)
+
+triangle[0][(n+n-1)//2] = 1
+
+for i in range(1, n):
+    for j in range(n+n-1):
+        if j == 0:
+            triangle[i][j] = triangle[i-1][j+1]
+        elif j == n+n-2:
+            triangle[i][j] = triangle[i-1][j-1]
+        else:
+            triangle[i][j] = triangle[i-1][j+1] + triangle[i-1][j-1]
+
+for i, lst in enumerate(triangle):
+    triangle[i] = '|'.join(list(map(str, filter(lambda x: x != 0, lst)))).center(n*2)
+
+triangle: str = '\n'.join(triangle)
+print(triangle)
